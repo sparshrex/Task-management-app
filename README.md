b6dadd2c53d985f518dc5d5848681088f066db73
Sparsh <res.sparsh@gmail.com>
2024-10-19 14:30:23 +0530

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,33 @@
-# Getting Started with Create React App
+# Task Management App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+A simple task management application built with React that allows users to drag and drop tasks between different columns (To Do, In Progress, Done). This app utilizes the `react` library for drag-and-drop functionality.
 
-## Available Scripts
+## Features
 
-In the project directory, you can run:
+- **Drag and Drop**: Easily move tasks between different states using drag-and-drop.
+- **Dynamic Task Rendering**: Tasks are dynamically rendered based on their state.
+- **User Avatars**: Each task can display team members with their avatars.
 
-### `npm start`
+## Technologies Used
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- React
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+-Redux
+-React draggable
+- Tailwind CSS (for styling)
+- JavaScript (ES6+)
 
-### `npm test`
+## Installation
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+1. Clone the repository:
 
-### `npm run build`
+   
+   git clone -
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+   cd task-management-app
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+ 2.  npm install
 
-### `npm run eject`
+  3.  npm start
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTask, updateTask } from './redux/taskSlice';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import TaskSection from './components/TaskSection';
+import avatar1 from './assets/Ellipse12.png';
+import avatar2 from './assets/Ellipse14.png';
+import avatar3 from './assets/Ellipse15.png';
+import groupimage from './assets/Group 646.png'
 
 function App() {
+  const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.tasks);
+  const [filter, setFilter] = useState('All');
+
+  // Dummy task data
+  const dummyTasks = [
+    {
+      id: 1,
+      title: 'Brainstorming',
+      priority: 'Low',
+      priorityColor: 'yellow',
+      description: "Brainstorming brings team members' diverse experience into play.",
+      comments: 12,
+      files: 0,
+      members: [
+        { id: 1, name: 'John', avatar: avatar1 },
+        { id: 2, name: 'Doe', avatar: avatar2 },
+        { id: 3, name: 'Jane', avatar: avatar3 },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Design Review',
+      priority: 'High',
+      priorityColor: 'red',
+      description: 'Review the design mockups for the mobile app.',
+      comments: 5,
+      files: 2,
+      members: [
+        { id: 1, name: 'John', avatar: avatar1 },
+        { id: 2, name: 'Jane', avatar: avatar3 },
+      ],
+    },
+    {
+      id: 3,
+      title: 'API Integration',
+      priority: 'Medium',
+      priorityColor: 'orange',
+      description: 'Integrate the new API for user authentication.',
+      comments: 3,
+      files: 1,
+      members: [
+        { id: 1, name: 'Doe', avatar: avatar2 },
+        { id: 2, name: 'Jane', avatar: avatar3 },
+      ],
+    },
+  ];
+
+  useEffect(() => {
+    if (tasks.length === 0) {
+      dummyTasks.forEach((task) => {
+        dispatch(addTask(task));
+      });
+    }
+  }, [dispatch, tasks.length]);
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleTaskDrop = (task, newPriority) => {
+   
+    const updatedTask = { ...task, priority: newPriority }; 
+    dispatch(updateTask(updatedTask));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" flex  h-screen">
+      <Sidebar />
+      <div className="flex-1 bg-white overflow-auto">
+        <Header />
+        <div>
+          <div className=' flex justify-between m-4'>
+          <h1 className="text-4xl font-semibold">Mobile App</h1>
+          <img src={groupimage} alt="icon" className='cursor-pointer' />
+
+          </div>
+          <div className=' flex  justify-between m-1'>
+          <select onChange={handleFilterChange} className= "bg-gray-100  border-2  border-gray-400 p-2 rounded- cursor-pointer">
+            <option value="All">Filter</option>
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+          </select>
+
+          <button className=' border-2  border-gray-400 bg-gray-100 mr-4 w-24  p-2 rounded-md' >Share</button>
+          </div>
+        </div>
+        <div className="p-6 grid grid-cols-3 gap-6">
+          <TaskSection title="To Do" tasks={tasks} filter={filter} color="border-purple-400" onDrop={(task) => handleTaskDrop(task, 'Low')} />
+          <TaskSection title="On Progress" tasks={tasks} filter={filter} color="border-orange-400" onDrop={(task) => handleTaskDrop(task, 'Medium')} />
+          <TaskSection title="Done" tasks={tasks} filter={filter} color="border-green-400" onDrop={(task) => handleTaskDrop(task, 'High')} />
+        </div>
+      </div>
     </div>
   );
 }
